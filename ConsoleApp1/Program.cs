7d08f7b8c6eb44348ecd6c3e48a6581fc773d546
Leone <leon.fajardoperalta@elev.ga.ntig.se>
2024-09-04 14:58:07 +0200

    temu kod interactiv

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,27 @@
-﻿Console.WriteLine("är du lång");
-string tall = Console.ReadLine();
+﻿Console.WriteLine("arm ett eller arm två?");
+string arm = Console.ReadLine(); // gör en variabel
 
-if (tall == "ja")
+
+if (arm == "arm ett") // om variablen som läses ut av readline blir arm ett
 {
-    Console.WriteLine("oj!");
+    Console.WriteLine("har arm ett fingrar??");
+
+}
+else // om inte
+{
+ Console.WriteLine("har arm två fingrar!!"); // skriv andra senariot 
+}
+
+string arm2 = Console.ReadLine();
+if (arm2 == "ja")
+{
+
+Console.WriteLine("wow du har fingrar!");
 
 }
 
+else 
+{
+    Console.WriteLine("köp nya");
+}
 Console.ReadLine();
